17245d7472583ab683432e0d3c7f0ae5fd3a2f07
AgungAwan <agungkristiawan24@gmail.com>
2024-03-06 00:03:24 +0800

    feat: implement custom networking

--- variables.tf
+++ variables.tf
@@ -13,6 +13,43 @@ variable "region" {
   default     = "us-east-1"
 }
 
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+  default     = "app_vpc"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+  default     = "app_subnet"
+
+}
+
+variable "igw_name" {
+  description = "Name of the internet gateway"
+  type        = string
+  default     = "app_igw"
+}
+
+variable "route_table_name" {
+  description = "Name of the route table"
+  type        = string
+  default     = "app_route_table"
+}
+
 variable "ami" {
   description = "AMI to use for the instance"
   default     = "ami-0cd59ecaf368e5ccf" // aws us-east-1 Canonical, Ubuntu, 20.04 LTS, amd64 focal image build on 2024-02-28
@@ -29,3 +66,4 @@ variable "instance_name" {
   default     = "app_server"
 
 }
+
